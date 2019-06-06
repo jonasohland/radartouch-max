0e7ecf5d011474a8bdbd3cbbdc21b5ceeafebf6c
Jonas Ohland <jonas.ohland@gmail.com>
2019-06-06 18:25:24 +0200

    added oscpp and switched to typesafe-get branch on min-api

--- source/radartouch/radartouch.cpp
+++ source/radartouch/radartouch.cpp
@@ -1,25 +1,41 @@
 #include <c74_min.h>
 #include <functional>
+#include <boost/asio.hpp>
+#include <oscpp/server.hpp>
 
-class radartouch : public c74::min::object<radartouch> {
 
-  public:
-    radartouch(const c74::min::atoms& args = {}) {}
+namespace o {
 
-    c74::min::inlet<> data_in{this, "data in"};
+    class radartouch : public c74::min::object<radartouch> {
 
-    c74::min::outlet<> data_out{this, "data out"};
-    c74::min::outlet<> ids_out{this, "ids out"};
-    c74::min::outlet<> shit_out{this, "shit out"};
+      public:
+        radartouch(const c74::min::atoms& args = {}) {
+            
+            if(args.size() <= 0)
+                return;
+            
+            auto args_it = args.begin();
+            
+            try {
+                
+                port = c74::min::atom::get<long>(*args_it);
+                
+                if(++args_it != args.end())
+                    max_blobs = c74::min::atom::get<long>(*args_it);
+                
+                cout << "Port: " << port << " Max Blobs: " << max_blobs << c74::min::endl;
+                
+            } catch (std::exception ex){
+                cerr << ex.what() << c74::min::endl;
+            }
+            
+            
+        }
+        
+        short port;
+        long max_blobs = -1;
+        
+    };
 
-    c74::min::atoms test(const c74::min::atoms& args, int inlet) {
-        return args;
-    }
-
-    c74::min::message<c74::min::threadsafe::no> reset_msg{
-        this, "reset", std::bind(&radartouch::test, this, std::placeholders::_1,
-                                 std::placeholders::_2),
-        "reset all data"};
-};
-
-MIN_EXTERNAL(radartouch);
+}
+MIN_EXTERNAL(o::radartouch);
