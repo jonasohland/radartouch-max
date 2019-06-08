2800a9fdaf7e26eabf0638fa685b2e7da8477312
Jonas Ohland <jonas.ohland@gmail.com>
2019-06-08 22:45:10 +0200

    radartouch_receiver impl

---
+++ source/shared/radartouch_message.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace o {
+
+    class radartouch_message {
+
+        struct blob {
+
+            blob(long bid)
+                : bid(bid) {}
+
+            long bid;
+            std::array<float, 5> args;
+            bool init = false;
+            
+            bool operator==(const blob& other) const {
+                return other.bid == bid && other.args == args && other.init == init;
+            }
+            
+            bool operator!=(const blob& other) const {
+                return !(*this == other);
+            }
+            
+            void set_args(std::array<float, 5>&& bargs){
+                args = bargs;
+                init = true;
+            }
+            
+            bool initialized() { return init; }
+        };
+
+      public:
+        
+        radartouch_message() = default;
+
+        radartouch_message(const radartouch_message& other) = default;
+
+        radartouch_message(radartouch_message&& other)
+            : blobs_(std::move(other.blobs_)), init_(other.init_) {
+            other.init_ = false;
+        }
+        
+        bool operator==(const radartouch_message& other) const {
+            return other.blobs_ == blobs_ && other.init_ == init_;
+        }
+        
+        bool operator!=(const radartouch_message& other) const {
+            return !(*this == other);
+        }
+
+        void set_alive_ids(std::vector<long> ids) {
+
+            for (long _id : ids)
+                blobs_.emplace_back(_id);
+            
+            init_ = true;
+            
+        }
+
+        void add_blob_args(long bid, std::array<float, 5>&& args) {
+
+            if (!blobs_.size()) return;
+
+            auto it = std::find_if(blobs_.begin(), blobs_.end(),
+                                   [&](auto blb) { return blb.bid == bid; });
+
+            if (it != blobs_.end())
+                (*it).set_args(std::forward<std::array<float, 5>>(args));
+        }
+
+        void reset() {
+            init_ = false;
+            blobs_.clear();
+        }
+
+        std::vector<blob>& blobs() { return blobs_; }
+
+        bool valid() {
+            
+            if(!init_) return false;
+            
+            auto last_ninit = std::find_if(blobs_.begin(), blobs_.end(),
+                         [](auto blob) { return !blob.initialized(); });
+            
+            return last_ninit == blobs_.end();
+            
+        }
+
+      private:
+        std::vector<blob> blobs_;
+        bool init_ = false;
+    };
+}
--- source/shared/radartouch_receiver.h
+++ source/shared/radartouch_receiver.h
@@ -2,3 +2,174 @@
 
 #include <o.h>
 #include <oscpp/server.hpp>
+#include "radartouch_message.h"
+
+namespace o {
+
+    using udp_device = o::io::net::datagram_device<boost::asio::ip::udp,
+                                                   std::string, o::ccy::strict>;
+
+    using app = o::io::io_app_base<o::ccy::strict>;
+
+    class radartouch_receiver : public app, public udp_device {
+
+      public:
+        radartouch_receiver() : udp_device(this->context()) {
+            this->app_launch();
+        }
+
+        ~radartouch_receiver() {
+
+            this->dgram_sock_close();
+
+            this->app_allow_exit();
+
+            this->app_join();
+        }
+
+        void set_init(bool is_init) { is_initialized = is_init; }
+
+        void bind(long port) { this->dgram_sock_bind(port); }
+        
+        virtual void handle_radartouch_msg(radartouch_message&& msg){
+            print_error_str("blob!");
+        }
+        
+        virtual void print_warn_str(std::string str){}
+
+        void handleBundle(const OSCPP::Server::Bundle& bundle) {
+
+            OSCPP::Server::PacketStream packets{bundle.packets()};
+
+            while (!packets.atEnd()) {
+
+                auto next = packets.next();
+
+                if (next.isMessage())
+                    handleMessage(next);
+                else
+                    handleBundle(next);
+            }
+        }
+
+        void handleMessage(const OSCPP::Server::Message& msg) {
+
+            if (strcmp(msg.address(), "/tuio/2Dcur") != 0) return;
+
+            if (msg.args().atEnd()) return;
+            
+            auto margs = msg.args();
+            
+            try {
+
+                auto ty = margs.string();
+
+                if (strcmp(ty, "alive") == 0) {
+                    
+                    c_msg_.reset();
+                    
+                    std::vector<long> args;
+                    
+                    while(!margs.atEnd())
+                        args.push_back(margs.int32());
+                    
+                    c_msg_.set_alive_ids(args);
+                    
+                    
+                } else if (strcmp(ty, "set") == 0) {
+
+                    std::array<float, 5> args;
+                    
+                    int bid = margs.int32();
+
+                    auto args_it = args.begin();
+
+                    do {
+                        *args_it = margs.float32();
+                    } while (++args_it != args.end() && !margs.atEnd());
+                    
+                    c_msg_.add_blob_args(bid, std::move(args));
+
+                } else if (strcmp(ty, "fseq") == 0) {
+                    
+                    if (!c_msg_.valid()) return;
+                    
+                    long current_fseq = margs.int32();
+
+                    if (!fseq)
+                        fseq = current_fseq;
+                    
+                    else if (current_fseq > fseq) {
+                        
+                        if (!(current_fseq == (fseq + 1))){
+                            
+                            std::stringstream err;
+                            
+                            err << "lost " << current_fseq - fseq << " packets";
+                            
+                            print_warn_str(err.str());
+                        }
+                        
+                        fseq = current_fseq;
+                        
+                    } else {
+                        
+                        if ((fseq - current_fseq) > 20)
+                            fseq = current_fseq;
+                        else
+                            c_msg_.reset();
+                    }
+                    
+                    handle_radartouch_msg(std::move(c_msg_));
+                    
+                    c_msg_.reset();
+                }
+                
+            } catch (OSCPP::UnderrunError err) {
+                print_error_str("UnderrunError");
+            } catch(OSCPP::ParseError err){
+                print_error_str("ParseError");
+            }
+            
+            
+        }
+
+        void on_dgram_received(std::string&& data) final {
+
+            auto pck = OSCPP::Server::Packet(data.data(), data.size());
+
+            if (pck.isBundle())
+                handleBundle(OSCPP::Server::Bundle(pck));
+            else if (pck.isMessage())
+                handleMessage(OSCPP::Server::Message(pck));
+            else
+                print_error_str("Could not parse data");
+        }
+
+        void on_dgram_sent() final {}
+
+        void on_dgram_error(udp_device::error_case eca,
+                            boost::system::error_code ec) final {
+
+            switch (eca) {
+            case udp_device::error_case::bind:
+                break;
+            case udp_device::error_case::connect:
+                break;
+            case udp_device::error_case::read:
+                break;
+            }
+
+            print_error_str(ec.message());
+
+            if (!is_initialized) throw std::runtime_error(ec.message());
+        }
+
+        virtual void print_error_str(std::string str) {}
+
+        bool is_initialized = false;
+
+        radartouch_message c_msg_;
+        long fseq = 0;
+    };
+};
