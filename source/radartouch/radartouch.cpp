ababd9107c009f79294e60830240f51e1f8cbe8c
Jonas Ohland <jonas.ohland@gmail.com>
2019-06-09 18:58:50 +0200

    formatting and nodesout message

--- source/radartouch/radartouch.cpp
+++ source/radartouch/radartouch.cpp
@@ -4,10 +4,10 @@
 
 namespace o {
 
-    class radartouch : public c74::min::object<radartouch>, public radartouch_receiver {
+    class radartouch : public c74::min::object<radartouch>,
+                       public radartouch_receiver {
 
-    public:
-
+      public:
         using b_outlet = c74::min::outlet<c74::min::thread_check::any,
                                           c74::min::thread_action::fifo>;
 
@@ -18,7 +18,7 @@ namespace o {
             if (args.size() <= 0) throw std::runtime_error("No port specified");
 
             auto args_it = args.begin();
-            
+
             long port;
 
             try {
@@ -39,67 +39,77 @@ namespace o {
                         outlets_.emplace_back(std::make_unique<b_outlet>(
                             this, outlet_name.str(), "list"));
                     }
-                    
+
                     outlet_states.resize(max_blobs);
-                    
-                    std::fill_n(outlet_states.begin(), max_blobs, std::make_tuple(-1, false, false));
+
+                    std::fill_n(outlet_states.begin(), max_blobs,
+                                std::make_tuple(-1, false, false));
+
+                    for (++args_it; args_it != args.end(); ++args_it) {
+                        if (c74::min::atom::get<std::string>(*args_it) ==
+                            "nodesout")
+                            nodes_out_ = std::make_unique<b_outlet>(
+                                this, "nodes outlet", "list");
+                    }
 
                 } else {
 
-/*                    outlets_.push_back(std::make_unique<b_outlet>(
-                        this, "Output (list)", "list")); */
-                    
+                    /*                    outlets_.push_back(std::make_unique<b_outlet>(
+                                            this, "Output (list)", "list")); */
+
                     throw std::runtime_error("No max blobs specified");
                 }
 
             } catch (c74::min::bad_atom_access ex) {
                 throw std::runtime_error(ex.what());
             }
-            
-            this->bind(port);
 
+            this->bind(port);
 
             do_read();
 
             this->set_init(true);
         }
 
-        ~radartouch(){}
-        
+        ~radartouch() {}
+
         void print_error_str(std::string str) final {
             cerr << str << c74::min::endl;
         }
-            
+
         void print_warn_str(std::string str) final {
             cwarn << str << c74::min::endl;
         }
-            
+
         void handle_radartouch_msg(radartouch_message&& msg) final {
-            
+
             std::vector<std::pair<long, radartouch_message::blob>> output;
-            
+
             std::vector<radartouch_message::blob> new_blobs;
-            
+
             std::vector<long> alive_indc;
-            
+
             std::vector<long> revived_indc;
 
             for (auto& blob : msg.blobs()) {
 
                 // find the position where this blob was sent out last
-                auto last_outlet_it = std::find_if(
-                    outlet_states.begin(), outlet_states.end(), [&](auto o_ls_id) {
-                        return std::get<0>(o_ls_id) == blob.bid;
-                    });
+                auto last_outlet_it =
+                    std::find_if(outlet_states.begin(), outlet_states.end(),
+                                 [&](auto o_ls_id) {
+                                     return std::get<0>(o_ls_id) == blob.bid;
+                                 });
 
                 // add to output list if id was found
-                if (last_outlet_it != outlet_states.end()){
-                    
-                    output.push_back(std::make_pair(last_outlet_it - outlet_states.begin(), blob));
-                    
-                    if(!std::get<2>(*last_outlet_it))
-                        revived_indc.push_back(last_outlet_it - outlet_states.begin());
-                    
+                if (last_outlet_it != outlet_states.end()) {
+
+                    output.push_back(std::make_pair(
+                        last_outlet_it - outlet_states.begin(), blob));
+
+                    if (!std::get<2>(*last_outlet_it))
+                        revived_indc.push_back(last_outlet_it -
+                                               outlet_states.begin());
+
                     std::get<0>(*last_outlet_it) = blob.bid;
                     std::get<1>(*last_outlet_it) = true; // will output
                     std::get<2>(*last_outlet_it) = true; // active this time
@@ -107,21 +117,22 @@ namespace o {
                 // add to new blobs list if not
                 else
                     new_blobs.push_back(blob);
-                
             }
-            
+
             std::vector<long> died_indc;
-    
-            // find blobs that were active the last time but are missing this time
-            for(int i = 0; i < outlet_states.size(); ++i){
-                if( (!std::get<1>(outlet_states[i])) && std::get<2>(outlet_states[i]) ){
+
+            // find blobs that were active the last time but are missing this
+            // time
+            for (int i = 0; i < outlet_states.size(); ++i) {
+                if ((!std::get<1>(outlet_states[i])) &&
+                    std::get<2>(outlet_states[i])) {
                     died_indc.push_back(i);
                     std::get<2>(outlet_states[i]) = false;
                 }
             }
-            
+
             // assign new blobs to inactive outlets
-            for(auto& new_b : new_blobs){
+            for (auto& new_b : new_blobs) {
 
                 // find free outlet
                 auto it =
@@ -129,39 +140,38 @@ namespace o {
                                  [&](auto ind) { return !std::get<1>(ind); });
 
                 // add to output and alive list
-                if(it != outlet_states.end()){
-                    
+                if (it != outlet_states.end()) {
+
                     std::get<1>(*it) = true;
                     std::get<2>(*it) = true;
-                    
-                    std::get<0>(*it) = new_b.bid;
-                    
-                    long index = it - outlet_states.begin();
-                    
-                    alive_indc.push_back(index);
-                    
-                    output.push_back(std::make_pair(index, new_b));
 
+                    std::get<0>(*it) = new_b.bid;
+
+                    long index = it - outlet_states.begin();
+
+                    alive_indc.push_back(index);
+
+                    output.push_back(std::make_pair(index, new_b));
                 }
             }
-            
+
             // output "died" messages
-            for(auto ind : died_indc){
+            for (auto ind : died_indc) {
                 outlets_[ind]->send("died");
             }
-            
+
             // output "alive messages"
-            for(long ind : alive_indc){
+            for (long ind : alive_indc) {
                 outlets_[ind]->send("alive");
             }
-            
+
             // output "revive messages"
-            for(long ind : revived_indc){
+            for (long ind : revived_indc) {
                 outlets_[ind]->send("revive");
             }
-            
+
             // sort output hi - lo, so we output max-style from right to left
-            std::sort(output.begin(), output.end(), [&](auto lhs, auto rhs){
+            std::sort(output.begin(), output.end(), [&](auto lhs, auto rhs) {
                 return std::get<0>(lhs) > std::get<0>(rhs);
             });
 
@@ -173,19 +183,24 @@ namespace o {
                     out.second.args[4]);
             }
 
+            if (nodes_out_) {
+                for (auto& out : output)
+                    nodes_out_->send("setnode", static_cast<int>(out.first) + 1,
+                                     out.second.args[1], out.second.args[2]);
+            }
+
             // reset outlet active flags
-            for(auto& ols : outlet_states)
+            for (auto& ols : outlet_states)
                 std::get<1>(ols) = false;
-            
-            
         }
-            
+
         // TODO: reset msg
 
-            
         long max_blobs = 0;
 
         c74::min::inlet<> inlet_{this, "commands input", "list"};
+
+        std::unique_ptr<b_outlet> nodes_out_;
 
         std::mutex blobs_mtx_;
 
